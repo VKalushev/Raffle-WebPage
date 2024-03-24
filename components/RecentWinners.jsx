9b5af4f94f7ba2440c43d47d2b9ae520346a495d
VKalushev <vasko.kalushev@gmail.com>
2024-03-24 12:39:03 +0000

    Update RecentWinners.jsx

--- components/RecentWinners.jsx
+++ components/RecentWinners.jsx
@@ -15,6 +15,7 @@ const RecentWinners = ({allRaffles, session}) => {
         }).map(raffle => {
             return {
                 username: formatUsername(raffle.winner),
+                host: raffle.ownerName,
                 prize: raffle.winning_prize,
                 participants: raffle.participants,
                 drawDate: formatDate(raffle.draw_date)
@@ -50,14 +51,16 @@ const RecentWinners = ({allRaffles, session}) => {
             {winners.length > 0 &&(
                 <div className="leaderboard-image border border-gray-300 rounded p-4 m-10 w-auto overflow-auto max-h-96">
                     <div className="font-bold text-lg mb-3 text-white">Winners From the last 7 days</div>
-                    <div className="grid grid-cols-4 gap-2">
+                    <div className="grid grid-cols-5 gap-2">
                         <div className="bg-gray-400 p-3 rounded-md">Username</div>
+                        <div className="bg-gray-400 p-3 rounded-md">Host</div>
                         <div className="bg-gray-400 p-3 rounded-md">Prize</div>
                         <div className="bg-gray-400 p-3 rounded-md">Out of Total Participants</div>
                         <div className="bg-gray-400 p-3 rounded-md">Draw Date</div>
                     {winners.map((winner, index) => (
                         <React.Fragment key={index}>
                         <div className="bg-white p-3 rounded-md">{winner.username}</div>
+                        <div className="bg-white p-3 rounded-md">{winner.host}</div>
                         <div className="bg-white p-3 rounded-md">{winner.prize}</div>
                         <div className="bg-white p-3 rounded-md">{winner.participants}</div>
                         <div className="bg-white p-3 rounded-md">{winner.drawDate}</div>
